07293d6e21ad24520a9512ec0367b157e4f0f1b0
user165280 <user165280@74-80-250-231.ic2net.net>
2020-02-25 16:13:24 -0800

    Added first set of TCs

---
+++ UITests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            //app.Repl();
+            app.Screenshot("First screen.");
+            app.Tap(x => x.Text("Bears"));
+            var result = app.Query(x => x.Text("American Black Bear"));
+            var targetText = "China";
+            var count = app.Query(x => x.Property("text").Contains(targetText)).Count();
+            Assert.That(count,Is.EqualTo(1), String.Format("There is no element that contain '%s'", targetText));
+        }
+    }
+}
