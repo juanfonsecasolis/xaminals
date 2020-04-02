e97cedb726e60d30a7591ffc999b28c18617b7c0
Juan Fonseca <juan.fonseca@gorillalogic.com>
2020-04-02 16:20:17 -0600

    Implemented POM

--- UITests/AppiumTests/Pages/AnimalPage.cs
+++ UITests/AppiumTests/Pages/AnimalPage.cs
@@ -3,8 +3,11 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Interactions;
 
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
 namespace AppiumTests.Pages
 {
+
     public class AnimalPage : BasePage
     {
         By backButtonLocator = ByAccessibilityId.XPath("//android.widget.ImageButton[@content-desc=\"OK\"]");
